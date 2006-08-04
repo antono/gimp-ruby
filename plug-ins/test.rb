a08c4f9791d3f8535708415bda100a69de7768ab
Scott Lembcke <lemb0029@gnome.org>
2006-08-04 07:46:19 +0000

    Added the GPL notification to all source files.
    2006-08-03  Scott Lembcke <lemb0029@gnome.org>
    
            * Added the GPL notification to all source files.
    
            * plug-ins/test.rb: Fixed a spelling error.

--- plug-ins/test.rb
+++ plug-ins/test.rb
@@ -1,5 +1,23 @@
 #!/usr/bin/env ruby
 
+# GIMP-Ruby -- Allows GIMP plugins to be written in Ruby.
+# Copyright (C) 2006  Scott Lembcke
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,Boston, MA
+# 02110-1301, USA.
+
 require 'rubyfu'
 
 RubyFu.register(
@@ -230,7 +248,7 @@ RubyFu.register(
   #  PDB.plug_in_script_fu_eval(Gimp::Interactive, '(ruby-fu-test-crash 123)')
   #end
 
-  printf 'Exucution error ... '
+  printf 'Execution error ... '
   test_exception(PDB::ExecutionError){PDB.ruby_fu_test_crash}
   
   Gimp.message('Tests ' + ($failure ? 'failed' : 'successful') + ":\n" + $stdout.string)
